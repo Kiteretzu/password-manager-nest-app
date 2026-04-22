1d64adac04f54edfe10732eb428f2b1f07233fee
smarth <smarthverma2003@gmail.com>
2026-04-22 08:59:53 -0700

    Add Users module with controller and service, including tests

---
+++ src/users/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+}
